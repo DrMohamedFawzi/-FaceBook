43fd180eb242e5ce93a55b22f2768c07f8f96961
DrMohamedFawzi <94241270+DrMohamedFawzi@users.noreply.github.com>
2022-05-28 12:06:39 +0300

    Initialize project using Create React App

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from '.'
 function Header() {
     return(
         <div className="header">
-            أه ب
+           Hi from header section.
+            <div className="header_left"></div>
+            <div className="header_middle"></div>
+            <div className="header_reight"></div>
 
         </div>
+
     )
 
 }
