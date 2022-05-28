15dc6626ffde218b4d2e053e8fa105521d76c565
DrMohamedFawzi <94241270+DrMohamedFawzi@users.noreply.github.com>
2022-05-28 21:50:25 +0300

    Initialize project using Create React App

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,35 @@
 import React from "react";
-import Header from '.'
+import "./Header.css"
+
 function Header() {
     return(
         <div className="header">
            Hi from header section.
-            <div className="header_left"></div>
-            <div className="header_middle"></div>
-            <div className="header_reight"></div>
+            <div className="header_left">
+
+            <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png"
+            alt="logo"
+
+
+            />
+
+            </div>
+            <div className="header_middle">
+
+                <input type="text"
+                placeholder="search facebook"
+                       className="header_input_text"
+
+                />
+
+
+            </div>
+            <div className="header_reight">
+
+
+
+            </div>
 
         </div>
 
